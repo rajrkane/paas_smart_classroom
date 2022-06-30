ecc66d7f905ee7138c24acf7fb4f9ad44836b025
Trey Manuszak <manuszaktrey@gmail.com>
2022-06-30 14:13:45 -0700

    This is the one that worked in testing.

--- handler.py
+++ handler.py
@@ -4,6 +4,7 @@ import pickle
 import face_recognition
 import urllib
 import io
+import json
 from os import system, listdir
 
 def open_encoding(filename):
@@ -18,22 +19,12 @@ def open_encoding(filename):
 def face_recognition_handler(event, context):
 	# Parse event
 	video_file_name = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-
-'''
-	# Get object from input bucket
-	s3 = S3()
-	try:
-		obj = s3.client.get_object(Bucket=s3.input_bucket, Key=video_file_name)
-	except Exception as e:
-		print(f"Could not get object {key} from bucket {s3.input_bucket}.") 
-		raise e 
-'''
+	
 	# Download the object as a file
+	s3 = S3()
 	path = "/tmp/"
 	video_file_path = path + video_file_name
-
-	print("hello")
-
+	
 	try:
 		s3.client.download_file(
 			Bucket=s3.input_bucket,
@@ -51,9 +42,7 @@ def face_recognition_handler(event, context):
 	# Get face from first image
 	image = face_recognition.load_image_file(str(path) + "image-001.jpeg")
 	unknown_encoding = face_recognition.face_encodings(image)[0]
-	print("Made it here")
-	return None
-'''
+
 	# Determine which face matches extracted image
 	encodings = open_encoding("/home/app/encoding")
 	for e in enumerate(encodings["encoding"]):
@@ -63,8 +52,8 @@ def face_recognition_handler(event, context):
 			break
 
 	# Get matching record from dynamodb
-	ddb = DDB()
-	try:
+	ddb = DDB() 
+	try: 
 		response = ddb.table.get_item(Key={"name": label})
 		print(f"Matched {video_file_name} to {response['Item']['name']}.")
 	except Exception as e:
@@ -74,23 +63,13 @@ def face_recognition_handler(event, context):
 
 	# Save result to output bucket
 	item = response["Item"]
-	save_name = video_file_name.split('.')[0] + ".txt"
-	save_body = f"{item['name']}\n{item['major']}\n{item['year']}"
+	save_name = video_file_name.split('.')[0]
+	save_body = f"{item['name']},{item['major']},{item['year']}"
 	with io.BytesIO() as f:
 		f.write(save_body.encode())
 		f.seek(0)
 		s3.client.upload_fileobj(f, s3.output_bucket, save_name)
-'''
-# def main():
-	# ddb = DDB()
-	# ddb.load_data("student_data.json")
-	# s3 = S3()
-	# s3.clear_input_bucket()
-	# s3.clear_output_bucket()
-	# print("Running Test Case 1")
-	# s3.upload_files("test_case_1")
-	# print("Running Test Case 2")
-	# s3.upload_files("test_case_2")
 
-# if __name__=="__main__":
-# 	main()
+	print("Upload successful.")
+
+
